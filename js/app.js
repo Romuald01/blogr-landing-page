b814d31a4f571e182e6086c7cbc327de08b54d78
Romuald01 <romualdadjimagbe2018@gmail.com>
2021-04-08 12:48:46 +0100

    testing blogr

---
+++ js/app.js
@@ -0,0 +1,27 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "95%";
+  document.getElementById("myNav").style.height= "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+// var iconHamburger = document.getElementById("icon-hamburger");
+// if(iconHamburger.click == true) {
+//   document.getElementById("icon-cancel").style.display
+// }
+
+// var a = true;
+// var b= 7;
+// if(a){
+//     console.log("a is equal to true");
+// }else{
+//     console.log("a is equal to false");
+
+// }
+// var x = 1 ; while ( x <= 10 ) { 
+
+//  document.writeln
+//  ("Thesquare of &nbsp;"+ x +"&nbsp;is &nbsp; &nbsp;"+  Math.pow(x,2)+"<br>"); 
+//  x = x + 1;
+// }
