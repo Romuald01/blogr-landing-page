241fbe9f066372af25ea73eeee5d1da88a911d46
Romuald01 <romualdadjimagbe2018@gmail.com>
2021-04-10 16:13:36 +0100

    testing blogr

--- js/app.js
+++ js/app.js
@@ -1,27 +1,20 @@
+ document.querySelector("#icon-close").style.display = "none";
+ var  $hamburgerIcon = document.getElementById("icon-hamburger");
+ var  $closeIconn = document.getElementById("icon-close");
 function openNav() {
-  document.getElementById("myNav").style.width = "95%";
-  document.getElementById("myNav").style.height= "100%";
-}
+  document.getElementById("myNav").style.width = "65%";
+  document.body.classList.add('no-scroll');
+  
+  $hamburgerIcon.style.display = 'none';
+  $closeIconn.style.display = 'block';
 
+  // $hamburgerIcon.classList.toggle.remove("hide");
+
+}
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
-}
-// var iconHamburger = document.getElementById("icon-hamburger");
-// if(iconHamburger.click == true) {
-//   document.getElementById("icon-cancel").style.display
-// }
-
-// var a = true;
-// var b= 7;
-// if(a){
-//     console.log("a is equal to true");
-// }else{
-//     console.log("a is equal to false");
-
-// }
-// var x = 1 ; while ( x <= 10 ) { 
-
-//  document.writeln
-//  ("Thesquare of &nbsp;"+ x +"&nbsp;is &nbsp; &nbsp;"+  Math.pow(x,2)+"<br>"); 
-//  x = x + 1;
-// }
+  document.body.classList.remove('no-scroll');
+  $hamburgerIcon.style.display = 'block';
+  $closeIconn.style.display = 'none';
+  
+ } 
